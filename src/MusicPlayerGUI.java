636b48ce98e10651e26461679e948f5870af9764
Ayhan Krala <codewolf27@gmail.com>
2024-01-07 18:08:58 +0300

    Implement toolbar with a 'Song' menu and 'Load Song' option

--- src/MusicPlayerGUI.java
+++ src/MusicPlayerGUI.java
@@ -1,25 +1,56 @@
 import javax.swing.*;
 
 public class MusicPlayerGUI extends JFrame {
-     public MusicPlayerGUI(){
-         //calls JFrame constructor to configure out and set the title to "Music Player"
-         super("Music Player");
+    public MusicPlayerGUI() {
+        //calls JFrame constructor to configure out and set the title to "Music Player"
+        super("Music Player");
 
-         //set the width and height
-         setSize(400,600);
+        //set the width and height
+        setSize(400, 600);
 
-         //end process when app is closed
-         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //end process when app is closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-         //launch the app at the center of the screen
-         setLocationRelativeTo(null);
+        //launch the app at the center of the screen
+        setLocationRelativeTo(null);
 
-         //present the app from being resized
-         setResizable(false);
+        //present the app from being resized
+        setResizable(false);
 
-         //set logout to null which always to control the (x,y) coordinates of our components
-         // also set the height and width
-         setLayout(null);
+        //set logout to null which always to control the (x,y) coordinates of our components
+        // also set the height and width
+        setLayout(null);
 
-     }
+        addGuiComponents();
+
+    }
+
+    private void addGuiComponents() {
+        // add toolbar
+        addToolbar();
+    }
+
+    private void addToolbar() {
+        JToolBar toolBar = new JToolBar();
+        toolBar.setBounds(0, 0, getWidth(), 20);
+
+        //prevent toolbar from being moved
+        toolBar.setFloatable(false);
+
+        //add drop down menu
+        JMenuBar menuBar = new JMenuBar();
+        toolBar.add(menuBar);
+
+        //now we will add a song menu where we will place the loading song option
+        JMenu songMenu = new JMenu("Song");
+        menuBar.add(songMenu);
+
+        //add the "Load song" item in the songMenu
+        JMenuItem loadSong = new JMenuItem("Load Song");
+        songMenu.add(loadSong);
+
+        add(toolBar);
+    }
+
+
 }
